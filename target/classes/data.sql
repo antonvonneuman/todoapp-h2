39599cd5f135331b72b1623a6ebc0d477099ceea
antox101 <anthony@hybridskill.com>
2022-07-31 17:21:09 +0530

    First Commit

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'Learn AWS', false, sysdate(), 'HybridSkill');
+insert into TODO
+values(10002, 'Learn GCP', false, sysdate(), 'HybridSkill');
+insert into TODO
+values(10003, 'Learn to DevOps', false, sysdate(), 'HybridSkill');
